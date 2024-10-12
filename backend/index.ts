bb05c26f83f37c727c8b13c1e3ff7d122a56a8cd
May Marie <lalanganph@gmail.com>
2024-10-12 07:45:14 +0200

    add: CORS requests fronm the frontend

--- backend/index.ts
+++ backend/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors({
-    origin: 'http://localhost:5174' 
+    origin: 'https://search-movies-1.onrender.com/' 
   }));
 app.use(express.json());
 
