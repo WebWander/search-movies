ac6a589be4f566b5294cc1ae65386da0acb6a434
May Marie <lalanganph@gmail.com>
2024-10-12 08:04:16 +0200

    add: Typescript Vite configuration file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,9 @@ import { configDefaults } from 'vitest/config'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', 
+  },
   base: '/search-movies/',
   test: {
     ...configDefaults,
