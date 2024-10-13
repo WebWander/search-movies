97875dad8a4010dfb73f93f3ed3487f86a29016a
May Marie <lalanganph@gmail.com>
2024-10-13 20:42:23 +0200

    WIP for deployment: use the netlify url

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -13,7 +13,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 4000;
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:5173' // Only allow requests from this origin
+    origin: 'https://movieflex1.netlify.app/' 
 }));
 app.use(express_1.default.json());
 // Routes
